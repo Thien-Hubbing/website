e87b2bbdd1297fe8102f87d383adc1b5930ce102
Thien-Hubbing <119017545+Thien-Hubbing@users.noreply.github.com>
2025-06-21 20:51:17 +0000

    Deploying to gh-pages from @ Thien-Hubbing/website@5625a96a9fa69a394af170b216b583e29028444f 🚀

--- +error.svelte
+++ +error.svelte
@@ -5,11 +5,11 @@
 		<link rel="icon" href="/website/favicon.ico" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
-		<link rel="modulepreload" href="/website/_app/immutable/entry/start.JAP-lK5t.js">
-		<link rel="modulepreload" href="/website/_app/immutable/chunks/CwXNgWwY.js">
+		<link rel="modulepreload" href="/website/_app/immutable/entry/start.S2Mxye-z.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/DNFlCNRA.js">
 		<link rel="modulepreload" href="/website/_app/immutable/chunks/qOy0IIMA.js">
 		<link rel="modulepreload" href="/website/_app/immutable/chunks/Cc0flOJJ.js">
-		<link rel="modulepreload" href="/website/_app/immutable/entry/app.D0J84OJ1.js">
+		<link rel="modulepreload" href="/website/_app/immutable/entry/app.DqGR2BRM.js">
 		<link rel="modulepreload" href="/website/_app/immutable/chunks/DXL290Nq.js">
 		<link rel="modulepreload" href="/website/_app/immutable/chunks/DQF29rDh.js">
 		<link rel="modulepreload" href="/website/_app/immutable/chunks/CUyEf6Yo.js">
@@ -18,7 +18,7 @@
 		<div style="display: contents">
 			<script>
 				{
-					__sveltekit_auefwd = {
+					__sveltekit_iyyrvj = {
 						base: "/website",
 						assets: "/website"
 					};
@@ -26,8 +26,8 @@
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("/website/_app/immutable/entry/start.JAP-lK5t.js"),
-						import("/website/_app/immutable/entry/app.D0J84OJ1.js")
+						import("/website/_app/immutable/entry/start.S2Mxye-z.js"),
+						import("/website/_app/immutable/entry/app.DqGR2BRM.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element);
 					});
