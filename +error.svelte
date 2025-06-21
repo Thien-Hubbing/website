4cad7640876790d04c3eacd3b27c1a56429ea416
Thien-Hubbing <119017545+Thien-Hubbing@users.noreply.github.com>
2025-06-21 16:10:26 +0000

    Deploying to gh-pages from @ Thien-Hubbing/website@133e5844be0c2a0533d25014f13ef09f3b372644 🚀

--- +error.svelte
+++ +error.svelte
@@ -2,32 +2,32 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<link rel="icon" href="/$(( github.event.repository.name ))/favicon.ico" />
+		<link rel="icon" href="/website/favicon.ico" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/start.CEEs-h1S.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/CHxecqqV.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/qOy0IIMA.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/Cc0flOJJ.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/app.D_K9_Sdu.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DXL290Nq.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DQF29rDh.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/CUyEf6Yo.js">
+		<link rel="modulepreload" href="/website/_app/immutable/entry/start.JAP-lK5t.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/CwXNgWwY.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/qOy0IIMA.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/Cc0flOJJ.js">
+		<link rel="modulepreload" href="/website/_app/immutable/entry/app.D0J84OJ1.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/DXL290Nq.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/DQF29rDh.js">
+		<link rel="modulepreload" href="/website/_app/immutable/chunks/CUyEf6Yo.js">
 	</head>
 	<body data-sveltekit-preload-data="hover">
 		<div style="display: contents">
 			<script>
 				{
-					__sveltekit_x8ik0g = {
-						base: "/$(( github.event.repository.name ))",
-						assets: "/$(( github.event.repository.name ))"
+					__sveltekit_auefwd = {
+						base: "/website",
+						assets: "/website"
 					};
 
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("/$(( github.event.repository.name ))/_app/immutable/entry/start.CEEs-h1S.js"),
-						import("/$(( github.event.repository.name ))/_app/immutable/entry/app.D_K9_Sdu.js")
+						import("/website/_app/immutable/entry/start.JAP-lK5t.js"),
+						import("/website/_app/immutable/entry/app.D0J84OJ1.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element);
 					});
