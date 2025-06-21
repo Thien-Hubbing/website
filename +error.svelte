5aaa202333049395541cdc24eeeb34f6265fe4b3
Thien-Hubbing <119017545+Thien-Hubbing@users.noreply.github.com>
2025-06-21 16:02:18 +0000

    Deploying to gh-pages from @ Thien-Hubbing/website@ec0aa3ac4b3ba24a9b9fbc55098362852bba1358 🚀

--- +error.svelte
+++ +error.svelte
@@ -5,11 +5,11 @@
 		<link rel="icon" href="/$(( github.event.repository.name ))/favicon.ico" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/start.CBQZCv9k.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/BaZrNhAv.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/start.CEEs-h1S.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/CHxecqqV.js">
 		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/qOy0IIMA.js">
 		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/Cc0flOJJ.js">
-		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/app.Cj31Jb7-.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/app.D_K9_Sdu.js">
 		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DXL290Nq.js">
 		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DQF29rDh.js">
 		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/CUyEf6Yo.js">
@@ -18,7 +18,7 @@
 		<div style="display: contents">
 			<script>
 				{
-					__sveltekit_2f310w = {
+					__sveltekit_x8ik0g = {
 						base: "/$(( github.event.repository.name ))",
 						assets: "/$(( github.event.repository.name ))"
 					};
@@ -26,8 +26,8 @@
 					const element = document.currentScript.parentElement;
 
 					Promise.all([
-						import("/$(( github.event.repository.name ))/_app/immutable/entry/start.CBQZCv9k.js"),
-						import("/$(( github.event.repository.name ))/_app/immutable/entry/app.Cj31Jb7-.js")
+						import("/$(( github.event.repository.name ))/_app/immutable/entry/start.CEEs-h1S.js"),
+						import("/$(( github.event.repository.name ))/_app/immutable/entry/app.D_K9_Sdu.js")
 					]).then(([kit, app]) => {
 						kit.start(app, element);
 					});
