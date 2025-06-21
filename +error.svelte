2500dd8db338830c6db6b41278e60fb4ad8b701d
nektos/act <nektos/act@users.noreply.github.com>
2025-06-21 15:57:23 +0000

    Deploying to gh-pages from @ Thien-Hubbing/website@b0d27dc8d3913a17450241048c5858ad0046b194 🚀

---
+++ +error.svelte
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="icon" href="/$(( github.event.repository.name ))/favicon.ico" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/start.CBQZCv9k.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/BaZrNhAv.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/qOy0IIMA.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/Cc0flOJJ.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/entry/app.Cj31Jb7-.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DXL290Nq.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/DQF29rDh.js">
+		<link rel="modulepreload" href="/$(( github.event.repository.name ))/_app/immutable/chunks/CUyEf6Yo.js">
+	</head>
+	<body data-sveltekit-preload-data="hover">
+		<div style="display: contents">
+			<script>
+				{
+					__sveltekit_2f310w = {
+						base: "/$(( github.event.repository.name ))",
+						assets: "/$(( github.event.repository.name ))"
+					};
+
+					const element = document.currentScript.parentElement;
+
+					Promise.all([
+						import("/$(( github.event.repository.name ))/_app/immutable/entry/start.CBQZCv9k.js"),
+						import("/$(( github.event.repository.name ))/_app/immutable/entry/app.Cj31Jb7-.js")
+					]).then(([kit, app]) => {
+						kit.start(app, element);
+					});
+				}
+			</script>
+		</div>
+	</body>
+</html>
