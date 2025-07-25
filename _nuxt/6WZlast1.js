fbf8d9cdc5c8a90ed07d4c6fc3d63356c43bacf5
Thien-Hubbing <119017545+Thien-Hubbing@users.noreply.github.com>
2025-07-25 22:21:34 +0000

    Deploying to gh-pages from @ Thien-Hubbing/website@247cefad6028f40c0d9c3553614bb81612da57df 🚀

---
+++ _nuxt/6WZlast1.js
@@ -0,0 +1,1 @@
+import{e as o,o as n,h as s,y as f,i as p}from"./C7aidoG5.js";const u={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const a=p;return n(),o(a,{href:t.href,target:t.target},{default:s(()=>[f(r.$slots,"default")]),_:3},8,["href","target"])}}};export{u as default};
--- _nuxt/a7Z03QmP.js
+++
@@ -1,1 +0,0 @@
-import e from"./CgKbvDxT.js";import{d as o,e as r,o as t}from"./D6nE1wLy.js";const s=o({__name:"index",setup(n){return(a,c)=>(t(),r(e))}});export{s as default};
